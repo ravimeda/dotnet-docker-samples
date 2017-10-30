e577d3a3a0ce4022e83f3352594641f2000ee7f9
Ravi Eda <raeda@microsoft.com>
2017-10-30 18:43:20 -0500

    Remove SampleLibrary

--- dotnetapp-prod/SampleLibrary/SampleDate.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace SampleLibrary
-{
-    internal static class SampleDateFormat
-    {
-        public static string GetDateTime() => DateTime.Now.ToString(format: "yyyy-MM-dd hh:mm:ss tt");
-    }
-}
